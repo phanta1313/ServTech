dba5833ddeb3138437eacdfeb4b82789d5bf8c44
phanta1313 <93367196+phanta1313@users.noreply.github.com>
2022-10-11 19:32:02 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+
+
+function categoryBtn1(){
+   if (document.getElementById('category1').style.display == 'block'){
+    document.getElementById('category1').style.display = 'none'
+    }
+    else{
+        document.getElementById('category1').style.display = 'block'
+    }
+
+    document.getElementById('filter1').style.display = 'none'
+}
+
+function categoryBtn2(){
+    if (document.getElementById('category2').style.display == 'block'){
+        document.getElementById('category2').style.display = 'none'
+    }
+    else{
+        document.getElementById('category2').style.display = 'block'
+    }
+
+    document.getElementById('filter2').style.display = 'none'
+}
+
+
+function categoryBtn3(){
+    if (document.getElementById('category3').style.display == 'block'){
+        document.getElementById('category3').style.display = 'none'
+    }
+    else{
+        document.getElementById('category3').style.display = 'block'
+    }
+
+    document.getElementById('filter3').style.display = 'none'
+}
+
+function filter1(){
+    if(document.getElementById('filter1').style.display == 'block') {
+        document.getElementById('filter1').style.display = 'none'
+    }
+    else{
+        document.getElementById('filter1').style.display = 'block'
+    }
+}
+
+function filter2(){
+    if(document.getElementById('filter2').style.display == 'block') {
+        document.getElementById('filter2').style.display = 'none'
+    }
+    else{
+        document.getElementById('filter2').style.display = 'block'
+    }
+}
+
+function filter3(){
+    if(document.getElementById('filter3').style.display == 'block') {
+        document.getElementById('filter3').style.display = 'none'
+    }
+    else{
+        document.getElementById('filter3').style.display = 'block'
+    }
+}
+
+
