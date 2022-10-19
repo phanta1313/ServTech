02930329550e9b1c7cabcd12791049bbd65fa7f1
phanta1313 <93367196+phanta1313@users.noreply.github.com>
2022-10-19 15:21:25 +0600

    Add files via upload

--- midtermProj/script.js
+++ midtermProj/script.js
@@ -1,65 +1,26 @@
 function categoryBtn1(){
-    let category1 = document.getElementById('category1');
-    let filter1 = document.getElementById('filter1');
-
-    let category1_result = (category1.style.display == 'block') ? category1.style.display = 'none' :
-    category1.style.display = 'block';
-
-    category1 = category1_result
-    
-    filter1.style.display = 'none'
+    $('#category1').toggleClass('hide');
 }
 
 function categoryBtn2(){
-    let category2 = document.getElementById('category2') 
-    let filter2 = document.getElementById('filter2')
-
-    let category2_result = (category2.style.display == 'block') ? category2.style.display = 'none' :
-    category2.style.display = 'block';
-
-    category2 = category2_result
-    
-    filter2.style.display = 'none'
+    $('#category2').toggleClass('hide');
 }
 
 
 function categoryBtn3(){
-    let category3 = document.getElementById('category3') 
-    let filter3 = document.getElementById('filter3')
-
-    let category3_result = (category3.style.display == 'block') ? category3.style.display = 'none' : 
-    category3.style.display = 'block';
-
-    category3 = category3_result
-    
-    filter3.style.display = 'none'
+    $('#category3').toggleClass('hide');
 }
 
 function filter1(){
-    let filter1 = document.getElementById('filter1') 
-
-    let filter1_result = (filter1.style.display == 'block') ? filter1.style.display = 'none' : 
-    filter1.style.display = 'block';
-
-    filter1 = filter1_result
+    $('#filter1').toggleClass('hide');
 }
 
 function filter2(){
-    let filter2 = document.getElementById('filter2') 
-
-    let filter2_result = (filter2.style.display == 'block') ? filter2.style.display = 'none' : 
-    filter2.style.display = 'block';
-
-    filter2 = filter2_result
+    $('#filter2').toggleClass('hide');
 }
 
 function filter3(){
-    let filter3 = document.getElementById('filter3') 
-
-    let filter3_result = (filter3.style.display == 'block') ? filter3.style.display = 'none' : 
-    filter3.style.display = 'block';
-
-    filter3 = filter3_result
+    $('#filter3').toggleClass('hide');
 }
 
 function openBasket(){
@@ -81,7 +42,7 @@ function goBack(){
 }
 
 category1 = {
-    
+
 }
 
 
