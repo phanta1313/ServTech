a369a0550902b142b3939c0f7e1b6a25ed163e7b
phanta1313 <93367196+phanta1313@users.noreply.github.com>
2022-10-22 18:11:26 +0600

    Add files via upload

--- midtermProj/script.js
+++ midtermProj/script.js
@@ -1,49 +1,37 @@
-function categoryBtn1(){
-    $('#category1').toggleClass('hide');
-}
+$('#open-category-button1').click(function(){
+    $('#category1').toggle();
 
-function categoryBtn2(){
-    $('#category2').toggleClass('hide');
-}
+})
 
+$('#open-category-button2').click(function(){
+    $('#category2').toggle();
+})
 
-function categoryBtn3(){
-    $('#category3').toggleClass('hide');
-}
+$('#open-category-button3').click(function(){
+    $('#category3').toggle();
+})
 
-function filter1(){
-    $('#filter1').toggleClass('hide');
-}
+$('#filter-button1').click(function(){
+    $('#filter1').toggle();
+})
 
-function filter2(){
-    $('#filter2').toggleClass('hide');
-}
+$('#filter-button2').click(function(){
+    $('#filter2').toggle();
+})
 
-function filter3(){
-    $('#filter3').toggleClass('hide');
-}
+$('#filter-button3').click(function(){
+    $('#filter3').toggle();
+})
 
-function openBasket(){
-    let basket_win = document.getElementById('basket-win')
-    let all_except_basket = document.querySelector('main')
-
-    basket_win_result = (basket_win.style.display == 'flex') ? basket_win.style.display = 'none' : 
-    basket_win.style.display = 'flex';
-
-    basket_win = basket_win_result
-
-    all_except_basket.style.display = 'none'
-
-}
-
-function goBack(){
-    document.getElementById('basket-win').style.display = 'none';
-    document.querySelector('main').style.display = 'block'
-}
+$('#openbasket').click(function(){
+    $('main').toggle();
+    $('#basket-win').toggle();
+})
 
 category1 = {
+  
+   }
 
-}
 
 
 
